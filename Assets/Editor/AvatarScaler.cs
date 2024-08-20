48a9dd4b8ef46142da2714447fe29d35987301a8
Macchiato <simppp@gmail.com>
2024-08-20 15:58:17 +0900

    리포지토리 초기화

---
+++ Assets/Editor/AvatarScaler.cs
@@ -0,0 +1,84 @@
+#if UNITY_EDITOR
+using System.Linq;
+
+using UnityEditor;
+using UnityEngine;
+
+using VRC.SDKBase;
+
+/*
+ * VRSuya AvatarScaler
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.avatarscaler {
+
+	public class AvatarScaler : MonoBehaviour {
+
+		private readonly static float Kikyo1mEyePosition = 0.88848f;
+
+		private static int UndoGroupIndex;
+
+		/// <summary>아바타의 키를 145cm에 맞춥니다.</summary>
+		[MenuItem("Tools/VRSuya/AvatarScaler/145cm", priority = 1200)]
+		public static void ScaleAvatar145cm() {
+			ScaleAvatar(145);
+			return;
+		}
+
+		/// <summary>아바타의 키를 160cm에 맞춥니다.</summary>
+		[MenuItem("Tools/VRSuya/AvatarScaler/160cm", priority = 1201)]
+		public static void ScaleAvatar160cm() {
+			ScaleAvatar(160);
+			return;
+		}
+
+		/// <summary>지정된 키를 목표로 아바타 스케일을 변경합니다.</summary>
+		private static void ScaleAvatar(int TargetHeight) {
+			if (GetVRCAvatar()) {
+				VRC_AvatarDescriptor AvatarDescriptor = GetVRCAvatar();
+				GameObject AvatarObject = AvatarDescriptor.gameObject;
+				Vector3 AvatarViewPosition = AvatarDescriptor.ViewPosition;
+				float TargetEyeHeight = Kikyo1mEyePosition * TargetHeight;
+				float TargetAvatarScale = TargetEyeHeight / AvatarViewPosition.y;
+				Undo.IncrementCurrentGroup();
+				Undo.SetCurrentGroupName("VRSuya AvatarScaler");
+				UndoGroupIndex = Undo.GetCurrentGroup();
+				ScaleAvatarTransform(AvatarObject, TargetAvatarScale);
+				ScaleAvatarViewPosition(AvatarDescriptor, TargetAvatarScale);
+				SceneView.RepaintAll();
+				Debug.Log("[AvatarScaler] " + AvatarObject.name + " 아바타 키를 " + TargetHeight + "cm으로 맞추었습니다!");
+			}
+			return;
+		}
+
+		/// <summary>아바타의 스케일을 변경합니다.</summary>
+		private static void ScaleAvatarTransform(GameObject TargetAvatar, float TargetScale) {
+			Transform TargetAvatarTransform = TargetAvatar.transform;
+			Undo.RecordObject(TargetAvatarTransform, "Changed Avatar Transform");
+			TargetAvatarTransform.localScale = TargetAvatarTransform.localScale * TargetScale;
+			EditorUtility.SetDirty(TargetAvatarTransform);
+			Undo.CollapseUndoOperations(UndoGroupIndex);
+		}
+
+		/// <summary>아바타의 뷰 포지션을 변경합니다.</summary>
+		private static void ScaleAvatarViewPosition(VRC_AvatarDescriptor TargetAvatarDescritor, float TargetScale) {
+			Undo.RecordObject(TargetAvatarDescritor, "Changed Avatar View Position");
+			TargetAvatarDescritor.ViewPosition = TargetAvatarDescritor.ViewPosition * TargetScale;
+			EditorUtility.SetDirty(TargetAvatarDescritor);
+			Undo.CollapseUndoOperations(UndoGroupIndex);
+		}
+
+		/// <summary>Scene에서 활성화 상태인 VRC AvatarDescriptor 컴포넌트를 가지고 있는 아바타 1개를 반환합니다.</summary>
+		/// <returns>활성화 상태인 VRC 아바타</returns>
+		private static VRC_AvatarDescriptor GetVRCAvatar() {
+			VRC_AvatarDescriptor[] AllVRCAvatarDescriptor = VRC.Tools.FindSceneObjectsOfTypeAll<VRC_AvatarDescriptor>().ToArray();
+			if (AllVRCAvatarDescriptor.Length > 0) {
+				return AllVRCAvatarDescriptor.Where(Avatar => Avatar.gameObject.activeInHierarchy).ToArray()[0];
+			} else {
+				return null;
+			}
+		}
+	}
+}
+#endif
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # AvatarScaler
+
+![Component](https://github.com/crestudio/AvatarScaler/blob/main/Image/VRSuya_AvatarScaler.jpg?raw=true)
+
+## 다운로드
+
+[VCC 패키지 추가](https://crestudio.notion.site/AvatarScaler)
+
+[UnityPackage 다운로드](https://github.com/crestudio/AvatarScaler/releases)
+
+---
+
+## 개요
+
 VRChat 아바타 자동 스케일링 프로그램
+
+---
+
+## 사용방법
+
+1. 사용 중인 Unity 프로젝트에 해당 패키지를 임포트합니다
+1. 키를 변경하고 싶은 **아바타를 하이어라키에서 선택**합니다
+1. Unity 메뉴 **Tools → VRSuya → AvatarScaler → 원하는 키**를 선택합니다
+
+---
+
+## 작동조건
+
+- [x] VRCSDK Avatar 3.0 패키지가 설치 및 올바르게 작동하고 있어야 합니다
+- [x] 아바타에 VRC 아바타 디스크립터가 설정되어 있어야 합니다
+
+---
+
+## Contact
+
+- Twitter : https://twitter.com/VRSuya
+- Mail : vrsuya@gmail.com
